4daa807e90f5aa2acad34e7f8ee71e02d4a62c74
Conrad Parker <conrad@metadecks.org>
2009-08-03 00:27:44 +0900

    add some haddock docs

--- UI/Command/Command.hs
+++ UI/Command/Command.hs
@@ -9,32 +9,36 @@ import Data.Default
 -- Command class
 --
 
+-- | A Command
 data Command = Command {
+    -- | Name of the program
     commandName :: String,
+
+    -- | Software version
     commandVersion :: String,
 
-    -- Email address to report bugs to
+    -- | Email address to report bugs to
     commandBugEmail :: String,
 
-    -- Names of authors
+    -- | Names of authors
     commandAuthors :: [String],
 
-    -- One-line description of the command
+    -- | One-line description of the command
     commandShortDesc :: String,
 
-    -- Long description of the command
+    -- | Long description of the command
     commandLongDesc :: String,
 
-    -- Categories to show in help text, in order of appearance
+    -- | Categories to show in help text, in order of appearance
     commandCategories :: [String],
 
-    -- Project that this command is part of
+    -- | Project that this command is part of
     commandProject :: String,
 
-    -- Related commands
+    -- | Related commands
     commandSeeAlso :: [String],
 
-    -- The actual subcommands
+    -- | The actual subcommands
     commandSubs :: [SubCommand]
 }
 
@@ -45,14 +49,37 @@ instance Default Command where
 -- SubCommand class
 --
 
+-- | It is often simpler to use the default implementation of SubCommand, and
+-- override it with the details you choose to use.
+-- For example, an implementation of the "hello world" command:
+--
+-- > world = def {
+-- >         subName = "world",
+-- >         subHandler = worldHandler,
+-- >         subCategory = "Greetings",
+-- >         subShortDesc = "An implementation of the standard software greeting."
+-- > }
+-- >
+-- > worldHandler :: [String] -> IO ()
+-- > worldHandler _ = putStrLn "Hello world!"
+--
 data SubCommand  = SubCommand {
+    -- | Name of the subcommand
     subName :: String,
+
+    -- | Handler
     subHandler :: [String] -> IO (),
+
+    -- | Category in this program's documentation
     subCategory :: String,
+
+    -- | Synopsis
     subSynopsis :: String,
+
+    -- | Short description
     subShortDesc :: String,
 
-    -- [(example description, args)]
+    -- | [(example description, args)]
     subExamples :: [(String, String)]
 }
 
@@ -60,3 +87,4 @@ instance Default SubCommand where
     def = SubCommand "<undocumented subcommand>"
                      (\_ -> putStrLn "Unimplemented command")
 		     def def def def
+
